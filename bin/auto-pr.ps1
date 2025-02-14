a7ae7c7b625e30ec9527e9ec68a92fc9ba397482
KT <git.encore609@8shield.net>
2025-02-14 14:46:39 +0800

    update

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "Lscodehub66/scoop-bucket:=main"
+    [String]$upstream = "<username>/<bucketname>:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
